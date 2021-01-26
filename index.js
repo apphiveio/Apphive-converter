db183536c58a56dd26168ac7b0cb63a00aa2539c
Jonatan Vazquez <xjonatan@gmail.com>
2021-01-25 22:45:46 -0600

    timeout express

--- index.js
+++ index.js
@@ -22,7 +22,6 @@ app.use(busboy());
 
 app.use('/', routes);
 
-app.use(timeout('15s'))
 
 app.use((req, res, next) => {
   res.status(404).send("Sorry can't find that!");
@@ -32,6 +31,13 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something broke!');
 });
+
+app.use(timeout(120000));
+app.use(haltOnTimedout);
+
+function haltOnTimedout(req, res, next){
+  if (!req.timedout) next();
+}
 
 const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, err => {
